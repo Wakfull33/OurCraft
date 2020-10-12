78f312ae8a0790e4aafd8fe1c2a7ca1a5b2cdf83
Wakfull33 <wakfulldu33@hotmail.fr>
2020-10-12 19:03:03 +0200

    Adding Items Data
    feat: adding Items Enums & Structs + Enums tools

--- Source/OurCraft/Utils/StructUtils.h
+++ Source/OurCraft/Utils/StructUtils.h
@@ -9,9 +9,43 @@
 /**
  * 
  */
+
+enum EItemType;
+
 UCLASS()
 class OURCRAFT_API UStructUtils : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
 	
 };
+
+#pragma region Items Structs
+
+USTRUCT(BlueprintType)
+struct FItemData
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
+	FString ItemName = "Default Name";
+
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
+	FString ItemDescription = "Default Description";
+
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
+	FString ItemMeshPath = "";
+
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
+	FString ItemIcon = "";
+
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
+	int ItemValue = 0;
+
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
+	FString ItemType;
+};
+
+
+#pragma endregion 
