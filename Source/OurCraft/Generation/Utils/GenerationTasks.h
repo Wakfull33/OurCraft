a1055652084688a2f336b5719bbc0bed7ba52b8e
BenoitEstival <benoit.estival@hotmail.com>
2020-10-12 16:23:56 +0200

    Generation setup

---
+++ Source/OurCraft/Generation/Utils/GenerationTasks.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "GenerationTasks.generated.h"
+
+/**
+ * 
+ */
+
+
+/*Library class for async task in generation*/
+UCLASS()
+class OURCRAFT_API UGenerationTasks : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
